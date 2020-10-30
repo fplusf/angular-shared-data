476e21f070b0808e7042a66a5cc1da6ea73b739e
Farrukh <fuzailof@gmail.com>
2020-10-30 16:56:09 +0500

    Home component:  Service state updates on setted interval.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,6 +5,7 @@ import {
   Renderer2,
   ViewChild,
 } from '@angular/core';
+import { StateService } from 'src/app/services/state-service.service';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +13,21 @@ import {
   styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent {
-  public currentTime: string;
-
   @ViewChild('time')
   public time: ElementRef;
 
-  constructor(private ngZone: NgZone, private renderer: Renderer2) {
+  constructor(
+    private ngZone: NgZone,
+    private renderer: Renderer2,
+    private stateService: StateService
+  ) {
     /**
      *  This code uses NgZone to run asynchronous calculation
      *  outside the Angular zone to optimize performance.
      */
     this.ngZone.runOutsideAngular(() => {
       ngZone.run(() => {
+        // Every second render new value to DOM element.
         setInterval(() => {
           this.renderer.setProperty(
             this.time.nativeElement,
@@ -31,12 +35,18 @@ export class HomeComponent {
             this.getCurrentTime()
           );
         }, 1000);
+
+        // Update state on State Service every 4 - seconds.
+        setInterval(
+          () => this.stateService.setTimeValue(this.getCurrentTime()),
+          4000
+        );
       });
     });
   }
 
   // Get current local time.
-  getCurrentTime() {
+  private getCurrentTime() {
     const today = new Date();
     return (
       today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
