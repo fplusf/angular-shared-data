4ac25648ffa0fd56bbe4529432b8de7bd89f1406
Farrukh <fuzailof@gmail.com>
2020-10-30 16:58:12 +0500

    All Components included to the App component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent} from './components/home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { HallComponent } from './components/hall/hall.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, HallComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
